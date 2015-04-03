a221969cf65808b6c5e6765d0e5cc353f599bbad
bruno <bruno@debian>
2015-04-03 14:41:12 -0300

    Enabled Spring

---
+++ com.sce.app/src/main/java/com/sce/app/Cliente.java
@@ -0,0 +1,15 @@
+package com.sce.app;
+
+public class Cliente {
+	
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
