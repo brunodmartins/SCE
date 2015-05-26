78bbd54be0371486678a9a45ddb4568e58ff7eaf
LeticiaSoares <lele_leticia.94@hotmail.com>
2015-05-25 22:45:27 -0300

    Tentativa carregar combo cursos

--- src/main/java/br/com/sce/aluno/AlunoController.java
+++ src/main/java/br/com/sce/aluno/AlunoController.java
@@ -1,8 +1,16 @@
+
 package br.com.sce.aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import br.com.sce.curso.Curso;
+import br.com.sce.curso.CursoController;
+import br.com.sce.curso.CursoService;
 import br.com.sce.service.IService;
 
 /**
@@ -11,24 +19,64 @@ import br.com.sce.service.IService;
  */
 @Controller
 @Scope("request")
-public class AlunoController{
+public class AlunoController {
 
-	@Autowired
-	private IService<Aluno> alunoService;
+	private IService<Aluno> alunoService;	
 	
+	private CursoService cursoService;
+
+	private List<Curso> itensCurso = new ArrayList<Curso>();
+
 	private Aluno aluno;
-	
+
+	private String confirmaSenha;
+
 	public AlunoController() {
-		aluno = new Aluno();
+		this.aluno = new Aluno();
+		this.alunoService = new AlunoService();		
+		this.cursoService=new CursoService();
 	}
-	
-	public void cadastrar(){
-		System.out.println("AAA");
-		try{
-			alunoService.salvarDados(aluno);				
-		}catch(Exception e){
-			//TODO
+
+	public List<Curso> selecionarCurso() {
+		try {
+			this.setItensCurso(cursoService.selecionarTodos());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		return this.getItensCurso();
+	}
+
+	public void cadastrar() {
+		if (aluno.getPassword().equals(this.getConfirmaSenha())) {
+			System.out.println("Senhas iguais!");
+		} else {
+			System.out.println("Senha não são iguais!");
+		}
+	}
+
+	public IService<Aluno> getAlunoService() {
+		return alunoService;
+	}
+
+	public void setAlunoService(IService<Aluno> alunoService) {
+		this.alunoService = alunoService;
+	}
+
+	public List<Curso> getItensCurso() {
+		return itensCurso;
+	}
+
+	public void setItensCurso(List<Curso> itensCurso) {
+		this.itensCurso = itensCurso;
+	}	
+
+	public String getConfirmaSenha() {
+		return confirmaSenha;
+	}
+
+	public void setConfirmaSenha(String confirmaSenha) {
+		this.confirmaSenha = confirmaSenha;
 	}
 
 	public Aluno getAluno() {
@@ -42,6 +90,5 @@ public class AlunoController{
 	public void setAlunoService(AlunoService alunoService) {
 		this.alunoService = alunoService;
 	}
-	
-	
+
 }
